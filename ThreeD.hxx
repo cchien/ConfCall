13e50b2e9e6e5eacbaeec1e347f25b6fdad95fae
PGao <mercenary90@gmail.com>
2011-06-29 14:36:36 -0700

    3D update

--- ThreeD.hxx
+++ ThreeD.hxx
@@ -10,8 +10,9 @@
 #include <bitset>
 #include <iostream>
 #include <fstream>
-#include "MyInputDataType.hxx"
-#include "MyOutputDataType.hxx"
+#include <CLAM/JACKNetworkPlayer.hxx>
+#include <CLAM/XMLStorage.hxx>
+
 
 namespace CLAM
 {
@@ -19,68 +20,55 @@ namespace CLAM
 class ThreeDConfig : public ProcessingConfig{
 
 public:
-	DYNAMIC_TYPE_USING_INTERFACE (ThreeDMixerConfig, 2, ProcessingConfig);
+	DYNAMIC_TYPE_USING_INTERFACE (ThreeDConfig, 2, ProcessingConfig);
 	DYN_ATTRIBUTE (0, public, int, ReceiverPositionX);
 	DYN_ATTRIBUTE (1, public, int, ReceiverPositionY);
 
 protected:	
 	void DefaultInit(void){
-        AddAll();
-        UpdateData();
-        unsigned int ReceiverPositionX = 0.0;
-		unsigned int ReceiverPositionY = 0.0;
-        
-    }
 
-}
+		Network network;
+		XMLStorage::Restore(network, "/home/rahul/Project/MultiParty/emacspace/SNS5/impulse-response-database-surround-to-stereo.clamnetwork");
+		//XMLStorage::Restore(network, "/home/christine/Project/sns5/windowing.clamnetwork");
+		//XMLStorage::Restore(network, argv[1]);	
+
+        	AddReceiverPositionX();
+		AddReceiverPositionY();
+
+        	UpdateData();
+
+		SetReceiverPositionX(0.0);
+		SetReceiverPositionY(0.0);
+
+		
+        
+	}
+
+};
 
 class ThreeD : public CLAM::Processing{
 
 private:
-    ThreeDConfig  mConfig;
+	ThreeDConfig configurator;
 	
 	AudioInPort _input;
-	AudioOutPort _leftOutput;
-	AudioOutPort _rightOutput;
+	AudioOutPort _output;
 	std::vector< FloatInControl* > mInputControls;
 	std::string _name;
 
 public:
 	ThreeD(const Config& config = Config()) 
 		: _input("Input", this)
-		, _leftOutput("Left Output", this) 
-		, _rightOutput("Right Output", this)
+		, _output("Output", this)
 	{
-		Configure( config );
-		
-/*******************************************************************/
-/*-----------------------------SETUP-------------------------------*/
-/*******************************************************************/
-		
-		CLAM::Network network;
-		try
-		{
-			CLAM::XMLStorage::Restore(network, "/home/rahul/Project/MultiParty/emacspace/SNS5/windowing.clamnetwork");
-			//CLAM::XMLStorage::Restore(network, "/home/christine/Project/sns5/windowing.clamnetwork");
-			//CLAM::XMLStorage::Restore(network, argv[1]);			
-		}
-		catch (CLAM::XmlStorageErr & e)
-		{
-			return error("Could not open the network file");
-		}
-		
-		CLAM::Processing& receiverPosition = network.GetProcessing("Receiver Position");
-		CLAM::SendFloatToInControl(receiverPosition, "DefaultX", ReceiverPositionX);
-		CLAM::SendFloatToInControl(receiverPosition, "DefaultY", ReceiverPositionY);
-		
+		Configure( configurator );
 	}
 
 	bool Do() //for each buffer
 	{
-		bool result = Do(mIn.GetData(), mOut.GetData()); 
-		
-		
+	
 		_input.Consume();
+		_output.Produce();
 		return true;
 	}
 
@@ -99,8 +87,6 @@ public:
 		return "ThreeD";
 	}
 
-	
-	
 
 };
 
